e36c2c4948b4698e3824e865c6af1f4c9cea4aca
matthewbillienyc <matthewbillienyc@gmail.com>
2015-12-08 20:58:48 -0500

    OOP solution to project euler 30, up to six digit numbers

---
+++ euler30.rb
@@ -0,0 +1,28 @@
+class EulerThirty
+
+  attr_reader :range, :answers
+
+  def initialize
+    @range = (2..354295)
+    @answers = []
+    check_each
+  end
+
+  def check_num(num)
+    arr = num.to_s.chars.map(&:to_i)
+    sum = arr.map { |n| n**5 }.inject(:+)
+    if num == sum
+      answers << num
+    end
+  end
+
+  def check_each
+    range.each {|n|
+      check_num(n)
+    }
+  end
+
+end
+
+project = EulerThirty.new
+puts project.answers
